fdb3afbaa026e0f87a027566510f1ee5aa9a0611
L7aur <gradlaur@yahoo.com>
2023-12-19 14:24:39 +0200

    hahahaxdd

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import io.qt.widgets.*;
+
+public class Main {
+    public static void main(String[] args) {
+        QApplication.initialize(args);
+        QMessageBox.information(null, "pula" , "pula");
+        QApplication.shutdown();
+    }
+}
