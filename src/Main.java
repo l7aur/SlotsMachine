396b3bcfaffa647263da00d7435647b29545a25b
L7aur <gradlaur@yahoo.com>
2023-12-19 14:35:52 +0200

    hahahaxdd

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import io.qt.widgets.*;
 public class Main {
     public static void main(String[] args) {
         QApplication.initialize(args);
-        QMessageBox.information(null, "penis" , "pen");
         QApplication.shutdown();
     }
 }
